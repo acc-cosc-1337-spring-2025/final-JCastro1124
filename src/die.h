f304ea420cfa886091a10b8d7b807457248a07d9
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-05-14 02:39:47 +0000

    Question 1

--- src/die.h
+++ src/die.h
@@ -1,1 +1,27 @@
 //
+#include <iostream>
+#include <vector>
+#include <random>
+
+
+using std::vector;
+
+#ifndef DIE
+#define Die
+class die
+{
+public:
+    int roll() {return sides[rand() %6];};
+
+
+
+private:
+    vector <int> sides {1,2,3,4,5,6}; 
+
+
+
+};
+
+#endif
+
+
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,21 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "die.h"
+#include <time.h>
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+
+
+TEST_CASE("Test roll")
+{
+	die game;
+	
+	srand(time(NULL));
+	for(int i = 0; i < 10; i++)
+	{
+		std::cout<<game.roll<<"\n";
+	}
 }
