41ae6cbdc6738ee4c2f377f63a8628233a397d15
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-05-14 20:09:24 +0000

    unfinished question 4

--- src/phase.h
+++ src/phase.h
@@ -1,1 +1,28 @@
 //
+#include "die.h"
+#include "roll.h"
+#include "shooter.h"
+#include <string>
+
+#ifndef PHASE
+#define PHASE
+
+class phase
+{
+public:
+    enum class RollOutcome(roll* rolled);
+
+
+
+private:
+
+
+
+
+};
+
+
+
+
+
+#endif
